748c862eeeae480541002738f7e61a9559f4ec42
fire9frenzy <tydeboon@gmail.com>
2015-03-02 20:52:23 -0700

    Added many more inputs

--- src/Simulation.java
+++ src/Simulation.java
@@ -11,58 +11,95 @@ public class Simulation
 		int years = 60;			// default one year
 		boolean variable = false;	// default constant cycle
 		int mastRate = 3;			// default 3 years
-		int density = -1;
+		int density = 150;
 		String fileName = "Data.txt";
 		boolean yearlyInfo = true;
+		int squirrelLow = 80;
+		int squirrelHigh = 90;
+		int seedsPerConeLow = 30;
+		int seedsPerConeHigh = 40;
+		int nutcrackerBoundry = 1000;
 		for (int i = 0; i < args.length; ++i)
 		{
-			args[i] = args[i].toLowerCase();
-			switch (args[i])
+			try
 			{
-				case "-s":
-					dimensions = 1000 * Integer.valueOf(args[++i]);
-					break;
-				case "-t":
-					numOfTrees = Integer.valueOf(args[++i]);
-					break;
-				case "-l":
-					if (!args[i + 1].equals("KP") && !args[i + 1].equals("PH"))
-						System.out.println("Location is unknown: using default settings");
-					else if (args[++i].equals("KP"))
-						location = "KP";
-					break;
-				case "-m":
-					maxCones = Integer.valueOf(args[++i]);
-					break;
-				case "-y":
-					years = Integer.valueOf(args[++i]);
-					// System.out.println("Years: " + years);
-					break;
-				case "-i":
-					if (!args[i + 1].equals("fixed") && !args[i + 1].equals("var"))
-						System.out.println("Mast year variability unknown: using defualts");
-					else if(args[++i].equals("var"))
-						variable = true;
-					mastRate = Integer.valueOf(args[++i]);
-					break;
-				case "-d":
-					density = Integer.valueOf(args[++i]);
-					break;
-				case "-o":
-					if (args[i + 1].equals("year"))
-						yearlyInfo = true;
-					else if (args[++i].equals("tree"))
-						yearlyInfo = false;
-					else
-						System.out.println("Unknown output type: using defualts");
-					break;
-				case "-f":
-					fileName = args[++i];
-					break;
-				case "-h":
-					printHelp();
-					System.exit(0);
-				default: break;
+				args[i] = args[i].toLowerCase();
+				switch (args[i])
+				{
+					case "-a":
+						dimensions = 1000 * Integer.valueOf(args[++i]);
+						break;
+					case "-t":
+						numOfTrees = Integer.valueOf(args[++i]);
+						break;
+					case "-l":
+						if (!args[i + 1].equals("KP") && !args[i + 1].equals("PH"))
+							System.out.println("Location is unknown: using default settings");
+						else if (args[++i].equals("KP"))
+							location = "KP";
+						break;
+					case "-m":
+						maxCones = Integer.valueOf(args[++i]);
+						break;
+					case "-y":
+						years = Integer.valueOf(args[++i]);
+						// System.out.println("Years: " + years);
+						break;
+					case "-i":
+						if (!args[i + 1].equals("fixed") && !args[i + 1].equals("var"))
+							System.out.println("Mast year variability unknown: using defualts");
+						else if(args[++i].equals("var"))
+							variable = true;
+						mastRate = Integer.valueOf(args[++i]);
+						break;
+					case "-d":
+						density = Integer.valueOf(args[++i]);
+						break;
+					case "-o":
+						if (args[i + 1].equals("year"))
+							yearlyInfo = true;
+						else if (args[++i].equals("tree"))
+							yearlyInfo = false;
+						else
+							System.out.println("Unknown output type: using defualts");
+						break;
+					case "-f":
+						fileName = args[++i];
+						break;
+					case "-c":
+						seedsPerConeLow = Integer.valueOf(args[++i]);
+						seedsPerConeHigh = Integer.valueOf(args[++i]);
+						if (squirrelLow >= squirrelHigh)
+						{
+							System.out.println("ConePerSeed low bound must be lower then the high bound: using defualts");
+							seedsPerConeLow = 30;
+							seedsPerConeHigh = 40;
+						}
+						break;
+					case "-s":
+						squirrelLow = Integer.valueOf(args[++i]);
+						squirrelHigh = Integer.valueOf(args[++i]);
+						if (squirrelLow >= squirrelHigh)
+						{
+							System.out.println("Squirrel low bound must be lower then the high bound: using defualts");
+							squirrelLow = 80;
+							squirrelHigh = 90;
+						}
+						break;
+						case "-n":
+							nutcrackerBoundry = Integer.valueOf(args[++i]);
+							break;
+					case "-h":
+						printHelp();
+						System.exit(0);
+					default: break;
+				}
+			}
+			catch(Exception e)
+			{
+				System.out.println("Error parsing input with string \"" + args[i] + "\"");
+				System.out.println("Exiting");
+				System.exit(0);
 			}
 		}
 		if (density > 0)
@@ -71,7 +108,7 @@ public class Simulation
 		// add pound
 		System.out.println("Simulation Parameters" + 
 							"\n  Area:      " + (dimensions / 1000) + "km^2" +
-							"\n  Trees:     " + numOfTrees +
+							"\n  Density:   " + density +
 							"\n  Years:     " + years + 
 							"\n  MastCycle: " + mastRate +
 							"\n  Location:  " + location +
@@ -85,7 +122,7 @@ public class Simulation
 		{
 			PrintWriter writer = new PrintWriter(fileName, "UTF-8");
 			if (yearlyInfo)
-				writer.println("source\tyear\tmastYear\ttrees\tconesProd\tconesEaten\tconeEscape\tcaches\tseedlings");
+				writer.println("source\tyear\tmastYear\ttrees\tconesProd\tconesEaten\tconeEscape\tconeEscapeDensity\tconeEscapePercent\tcaches\tseedlings");
 			else
 				writer.println("source\tconesProd");
 			int lastMastYear = 0;
@@ -138,6 +175,10 @@ public class Simulation
 					// cones eaten
 					writer.print(area.getYearConesEaten() + "\t");
 					// coneEscape
+					writer.print((area.getYearCones() - area.getYearConesEaten()) + "\t");
+					// coneEscapeDensity
+					writer.print((area.getYearCones() - area.getYearConesEaten()) / (dimensions / 1000) + "\t");
+					// coneEscapePercent
 					writer.print(1 - ((double)area.getYearConesEaten() / (double)area.getYearCones()) + "\t");
 					// caches
 					writer.print(area.getCacheCount() + "\t");
@@ -181,8 +222,8 @@ public class Simulation
 	public static void printHelp()
 	{
 		System.out.println("\nOption input\n   description\n");
-		System.out.println("-S int --default 2km\n   the size of the simulated area in kilometers squared.\n");
-		System.out.println("-T int --default 150\n   the number of trees in the simulated area\n");
+		System.out.println("-A int --default 2km\n   the size of the simulated area in kilometers squared.\n");
+		// System.out.println("-T int --default 150\n   the number of trees in the simulated area\n");
 		System.out.println("-L str --default PH\n   {PH|KP} determines which data to uses for cone production\n");
 		System.out.println("-M int --default no limit\n   the max number of cones a tree can produce\n");
 		System.out.println("-Y int --default 1\n   the total years the simulation will run for\n");
@@ -190,5 +231,8 @@ public class Simulation
 		System.out.println("-D int --default not set\n   three density in trees/km^2\n");
 		System.out.println("-O str --default year\n   {tree|year} tree gives info about trees. year gives info about each year\n");
 		System.out.println("-F str --default Data.txt\n   file name including suffix\n");
+		System.out.println("-S int int --default 80 90\n   low then high boundry for how much squirrels eat\n");
+		System.out.println("-C int int --default 30 40\n   the boundry for seeds from each cone\n");
+		System.out.println("-N int --default 100 \n   the boundry for nutcrackers appearing\n");
 	}
 }
